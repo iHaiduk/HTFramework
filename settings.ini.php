dc45950519c3075b2c372e71ec884d31f843a13b
Ihor Gayduk, Yaroslav Goisa <htgroup.dev@gmail.com>
2013-07-28 13:41:52 +0300

    Create settings.ini.php

---
+++ settings.ini.php
@@ -0,0 +1,9 @@
+[SQL]
+engine =        mysql
+host =          localhost
+;port =         3306
+user =          root
+password =
+dbname =        site_dating
+charset =       utf8
+tablePrefix =   ht_
